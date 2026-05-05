27f113230ef3b92e49c1d84c02ab93c3bb4f5c1a
Ha-Giang <michael.huy.2014@gmail.com>
2026-05-05 15:27:49 +0700

    ok

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jhebreoxwuimlqwvjdok.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpoZWJyZW94d3VpbWxxd3ZqZG9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3Nzc5MzAsImV4cCI6MjA5MDM1MzkzMH0.VSaOSgcEOZEQbiHP8-CxbyxOJPpJkF1gVIdcidb2rk4';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
